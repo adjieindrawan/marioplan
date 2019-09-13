12fbeb6dfe9f2a50e88b33a2ac545eed3f2305b8
adjieindrawan <adjieindrawan@gmail.com>
2019-09-13 10:42:19 +0700

    connection redux to firebase

--- src/index.js
+++ src/index.js
@@ -6,12 +6,22 @@ import "./style.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./store/reducers/rootReducer";
 import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
